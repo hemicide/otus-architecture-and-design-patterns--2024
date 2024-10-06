e8a848cc0a7b18be19e5ec4f5528b50e04e1b75b
hristov <com.hemicide@gmail.com>
2024-10-06 17:36:30 +0300

    init solid and exceptions handler

---
+++ ConsoleApp1/ExceptionHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ExceptionHandler
+    {
+        private static Dictionary<Type, Func<ICommand, Exception, ICommand>> _defaultStore = [];
+        private static readonly Dictionary<Type, IDictionary<Type, Func<ICommand, Exception, ICommand>>> _store = [];
+        public ExceptionHandler()
+        {
+            _defaultStore.Add(typeof(Exception), (c, e) => { return new ConsoleOutCommand(e); });
+        }
+        public static ICommand Handle(ICommand c, Exception e)
+        {
+            Type ct = c.GetType();
+            Type et = e.GetType();
+
+            var commandStore = _store.GetValueOrDefault(ct, _defaultStore);
+
+            return commandStore[et](c, e);
+        }
+
+        public static void RegisterHandler(Type ct, Type et, Func<ICommand, Exception, ICommand> h)
+        {
+            if (_store.ContainsKey(ct))
+                _store[ct][et] = h;
+            else
+            {
+                Dictionary<Type, Func<ICommand, Exception, ICommand>> _dict = [];
+                _dict.Add(et, h);
+                _store.Add(ct, _dict);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1Tests/ExceptionHandlerTests.cs
@@ -0,0 +1,148 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass]
+    public class ExceptionHandlerTests
+    {
+        /// <summary>
+        /// Реализовать Команду, которая записывает информацию о выброшенном исключении в лог.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_LogThrownException()
+        {
+            // Arrange
+            var mock = new Mock<ICommand>();
+            mock.Setup(c => c.Execute()).Callback(() => throw new IOException("IOException"));
+
+            CommandCollection.Add(mock.Object);
+
+            // Register handlers
+            ExceptionHandler.RegisterHandler(mock.Object.GetType(), typeof(IOException), (c, e) => { return new ConsoleOutCommand(e); });
+
+            // Act
+            CommandCollection.LoopUntilNotEmpty();
+
+            // Assert
+            mock.Verify(c => c.Execute(), Times.Exactly(1));
+        }
+
+        /// <summary>
+        /// Реализовать обработчик исключения, который ставит Команду, пишущую в лог в очередь Команд.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_RetryWithDelayCommand()
+        {
+            // Arrange
+            var mock = new Mock<ICommand>();
+            mock.Setup(c => c.Execute()).Callback(() => throw new IOException("IOException"));
+
+            CommandCollection.Add(mock.Object);
+
+            // Register handlers
+            ExceptionHandler.RegisterHandler(mock.Object.GetType(), typeof(IOException), (c, e) => { return new RetryWithDelayCommand(new ConsoleOutCommand(e)); });
+
+            // Act
+            CommandCollection.LoopUntilNotEmpty();
+
+            // Assert
+            mock.Verify(c => c.Execute(), Times.Exactly(1));
+        }
+
+        /// <summary>
+        /// Реализовать Команду, которая повторяет Команду, выбросившую исключение.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_RetryWithDelayCommandThrowExceptions()
+        {
+            // Arrange
+            var mock = new Mock<ICommand>();
+            mock.Setup(c => c.Execute()).Callback(() => throw new IOException("IOException"));
+
+            CommandCollection.Add(mock.Object);
+
+            // Register handlers
+            ExceptionHandler.RegisterHandler(mock.Object.GetType(), typeof(IOException), (c, e) => { return new RetryWithDelayCommand(c); });
+
+            // Act
+            CommandCollection.LoopPerCount(5);
+
+            // Assert
+            mock.Verify(c => c.Execute(), Times.Exactly(2));
+        }
+
+        /// <summary>
+        /// Реализовать обработчик исключения, который ставит в очередь Команду - повторитель команды, выбросившей исключение.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_RetryWithDelayNowCommandThrowExceptions()
+        {
+            // Arrange
+            var mock = new Mock<ICommand>();
+            mock.Setup(c => c.Execute()).Callback(() => throw new IOException("IOException"));
+
+            CommandCollection.Add(mock.Object);
+
+            // Register handlers
+            ExceptionHandler.RegisterHandler(mock.Object.GetType(), typeof(IOException), (c, e) => { return new RetryWithDelayNowCommand(c); });
+
+            // Act
+            CommandCollection.LoopPerCount(10);
+
+            // Assert
+            mock.Verify(c => c.Execute(), Times.Exactly(1));
+        }
+
+        /// <summary>
+        /// При первом выбросе исключения повторить команду, при повторном выбросе исключения записать информацию в лог.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_FirstRetryThenThrowExceptions()
+        {
+            // Arrange
+            var mock = new Mock<ICommand>();
+            mock.Setup(c => c.Execute()).Callback(() => throw new IOException("IOException"));
+
+            CommandCollection.Add(mock.Object);
+
+            // Register handlers
+            ExceptionHandler.RegisterHandler(mock.Object.GetType(), typeof(IOException), (c, e) => { return new RetryWithDelayCommand(new FirstRetryCommand(c)); });
+            ExceptionHandler.RegisterHandler(typeof(FirstRetryCommand), typeof(IOException), (c, e) => { return new ConsoleOutCommand(e); });
+
+            // Act
+            CommandCollection.LoopUntilNotEmpty();
+
+            // Assert
+            mock.Verify(c => c.Execute(), Times.Exactly(2));
+        }
+
+        /// <summary>
+        /// При первом выбросе исключения повторить команду два раза, при следующем выбросе исключения записать информацию в лог.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_SecondRetryThenThrowExceptions()
+        {
+            // Arrange
+            var mock = new Mock<ICommand>();
+            mock.Setup(c => c.Execute()).Callback(() => throw new IOException("IOException"));
+
+            CommandCollection.Add(mock.Object);
+
+            // Register handlers
+            ExceptionHandler.RegisterHandler(mock.Object.GetType(), typeof(IOException), (c, e) => { return new RetryWithDelayCommand(new FirstRetryCommand(c)); });
+            ExceptionHandler.RegisterHandler(typeof(FirstRetryCommand), typeof(IOException), (c, e) => { return new RetryWithDelayCommand(new SecondRetryCommand(c)); });
+            ExceptionHandler.RegisterHandler(typeof(SecondRetryCommand), typeof(IOException), (c, e) => { return new ConsoleOutCommand(e); });
+
+            // Act
+            CommandCollection.LoopUntilNotEmpty();
+
+            // Assert
+            mock.Verify(c => c.Execute(), Times.Exactly(3));
+        }
+    }
+}
