a3f3fc84c52d54e76b15773615c924afb0833729
hristov <com.hemicide@gmail.com>
2024-10-06 14:40:37 +0300

    init branch

---
+++ ConsoleApp1/FindSquare.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class FindSquare()
+    {
+        public double[] solve(double a, double b, double c, double e = 0.00001)
+        {
+            if (double.IsNaN(a))
+                throw new ArgumentException("a is not a number");
+            if (double.IsNaN(b))
+                throw new ArgumentException("b is not a number");
+            if (double.IsNaN(c))
+                throw new ArgumentException("c is not a number");
+
+            if (Math.Abs(a) <= e)
+                throw new ArgumentException("a должно быть не равно 0");
+
+            var D = (b * b) - (4 * (a * c));
+            if (D < -e)
+                return [];
+
+            if (Math.Abs(D) <= e)
+                return [-b/(2*a), -b/(2*a)];
+
+            if (D > e)
+                return [(-b + Math.Sqrt(D)) / (2 * a), (-b - Math.Sqrt(D)) / (2 * a)];
+
+            return [];
+        }
+    }
+}
---
+++ ConsoleApp1Tests/FindSquareTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class FindSquareTests
+    {
+
+        [TestMethod()]
+        public void solveTest()
+        {
+            var tests = new[]
+            {
+               new { name = "x^2+1 = 0", a = 1.0, b = 0.0, c = 1.0, want = (object)Array.Empty<double>() },
+               new { name = "x^2-1 = 0", a = 1.0, b = 0.0, c = -1.0, want = (object)new double[] { 1, -1 } },
+               new { name = "x^2+2x+1 = 0", a = 1.0, b = 2.0, c = 1.0, want = (object)new double[] { -1, -1 } },
+               new { name = "x^2-3x+2 = 0", a = 1.0, b = -3.0, c = 2.0, want = (object)new double[] { 2, 1 } },
+               new { name = "c check is NaN", a = 1.0, b = 0.0, c = Math.Sqrt(-4), want = (object)new ArgumentException("c is not a number") },
+               new { name = "b check is NaN", a = 1.0, b = Math.Sqrt(-4), c = 0.0, want = (object)new ArgumentException("b is not a number") },
+               new { name = "a check is NaN", a = Math.Sqrt(-4), b = 0.0, c = 0.0, want = (object)new ArgumentException("a is not a number") }
+            };
+
+            //Arrange
+            var square = new FindSquare();
+
+            foreach (var test in tests)
+            {
+                try
+                {
+                    //Act
+                    var result = square.solve(test.a, test.b, test.c, double.Parse("1E-5"));
+
+                    //Assert
+                    CollectionAssert.AreEqual(test.want as double[], result);
+                }
+                catch (Exception ex)
+                {
+                    //Assert
+                    Assert.AreEqual((test.want as Exception)?.Message, ex.Message);
+                }
+            }
+        }
+    }
+}
