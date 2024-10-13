b5fbdc5040a067c0e5c6a719656fd97108c98803
hristov <com.hemicide@gmail.com>
2024-10-13 19:14:50 +0300

    added UObject entity

---
+++ ConsoleApp1/Interfaces/IUObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBattle.Interfaces
+{
+    public interface IUObject
+    {
+        object GetProperty(string key);
+        void SetProperty(string key, object value);
+    }
+}
